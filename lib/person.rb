54364049158360306969d1233220286f39383a84
mjdele <mike.delehant@gmail.com>
2016-03-21 13:57:31 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+
+  def initialize(name_of_person)
+    @name = name_of_person
+  end
+
+  def name=(name_of_person)
+    @name = name_of_person
+  end
+
+  def name
+    @name
+  end
+
+end
